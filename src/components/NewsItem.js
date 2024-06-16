a31651c5442b198771ff93bd420423071106c840
akshitha-108 <akshithajnv@gmail.com>
2024-06-16 23:40:55 +0530

    adding the categories

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     let {title,description,imageUrl,newsUrl} = this.props;
     return (
         <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
         <img src={!imageUrl?"https://c.ndtvimg.com/2023-03/9n5mam7o_yediyurappa_625x300_30_March_23.jpg?im=FaceCrop,algorithm=dnn,width=650,height=400":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
