283d0cc15e00caaaa258df8f0999614fb784ae9d
akshitha-108 <akshithajnv@gmail.com>
2024-06-16 17:32:19 +0530

    chnages3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 
-// this is change in master branch
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
