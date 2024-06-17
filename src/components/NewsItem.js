3e3ae618c5d80caffa4e456185303403dd2655c5
akshitha-108 <akshithajnv@gmail.com>
2024-06-17 10:24:42 +0530

    adding badge

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
   render() {
-    let {title,description,imageUrl,newsUrl,author,date} = this.props;
+    let {title,description,imageUrl,newsUrl,author,date,source} = this.props;
     return (
         <div className='my-3'>
         <div className="card">
+        <span className='position-absolute top-0 translate-middle badge rounded-pill bg-danger' style={{left:'90%',zIndex:'1'}}>{source}</span>
         <img src={!imageUrl?"https://c.ndtvimg.com/2023-03/9n5mam7o_yediyurappa_625x300_30_March_23.jpg?im=FaceCrop,algorithm=dnn,width=650,height=400":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <p className='card-text'><small className='text-muted'>By {!author?"unknown":author} on {date}</small></p>
+          <p className='card-text'><small className='text-muted'>By {!author?"unknown":author} on {new Date(date).toGMTString()}</small></p>
           <a rel='noreferrer' href={newsUrl} target = "_blank"  className="btn btn-sm btn-dark">Read More</a>
         </div>
       </div>
